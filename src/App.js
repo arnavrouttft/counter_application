aaa9d1ef5a5ca6286d5d275a85dc37b81e8492b5
Arnav Rout <100131941+arnavrouttft@users.noreply.github.com>
2022-05-03 16:57:42 +0530

    background modified

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ function App() {
 
 
   return (
+    <div>
     <div className="App">
       <h2 className='mainHeading'>COUNTER APPLICATION</h2><br/>
       <h3 className="heading">Enter a number from where you want to start the count</h3>
       <input onChange={(e) => { setChange(e.target.value) }} type="number" name="number" placeholder="Enter a number..." />
       
       <Wrapper startFrom={Number(change)} />
+    </div>
     </div>
   );
 }
